9e60a906e1f55e9bd62f278b6b1141fbaab05736
kpatel1111 <124097247+kpatel1111@users.noreply.github.com>
2024-02-25 21:36:24 -0500

    added the minus function test inputs.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -15,4 +15,13 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1,1),2);
 	}
+	/*
+	 * The below test method is utilized to test the method in the simple calculator class because it will
+	 * receive two integer numbers and will verify if both the expected and the actual values are the same results.
+	 */
+	@Test
+	public void testMinus(){
+		SimpleCalculator calculator = new SimpleCalculator();
+		assertEquals(2,calculator.minus(5,3));
+	}
 }
