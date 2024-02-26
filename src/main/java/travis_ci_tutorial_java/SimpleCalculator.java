561af6b69814c61c1c72935b38cf491b9f261662
kpatel1111 <124097247+kpatel1111@users.noreply.github.com>
2024-02-25 21:38:44 -0500

    added the minus function method in the simple calculator java file.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,23 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
+	/*
+	 * The below test method is utilized to test the add method in the simple calculator class because it will
+	 * receive two integer numbers and verify if both the expected and the actual values are the same results.
+	 */
+	@Test
+	public void testAdd(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(1,1),2);
+	}
+	
+	/*
+	 * The below test method is utilized to test the method in the simple calculator class because it will
+	 * receive two integer numbers and will verify if both the expected and the actual values are the same results.
+	 */
+	@Test
+	public void testMinus(){
+		SimpleCalculator calculator = new SimpleCalculator();
+		assertEquals(2,calculator.minus(5,3));
 	}
 }
