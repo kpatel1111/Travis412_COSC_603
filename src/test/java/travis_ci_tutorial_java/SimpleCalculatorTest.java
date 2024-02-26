b5c67bd45bf27ce2c76100e59d72ae4b7faf9811
kpatel1111 <124097247+kpatel1111@users.noreply.github.com>
2024-02-25 21:16:28 -0500

    changed the add test method from 1+1=2 to 1+1=0, to check if the circle-ci software is producing the test inputs as required.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -6,9 +6,13 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SimpleCalculatorTest {
+	/*
+	 * The below test method is utilized to test the add method in the simple calculator class because it will
+	 * receive two integer numbers and verify if both the expected and the actual values are the same results.
+	 */
 	@Test
-	public void testAdd() {
+	public void testAdd(){
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1,1),0);
 	}
 }
